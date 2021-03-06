d295958cbcdebb9b1a24f86d43432af10d33c980
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-03-06 17:51:48 -0500

    established initial connection with employee_db

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+});
